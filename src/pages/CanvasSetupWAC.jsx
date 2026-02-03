278a566cf2fbae96a53ab8cb522be0fbfaa70f94
Nishad MP <nishad.mp@webandcrafts.com>
2026-02-03 12:31:52 +0530

    iteration-3

--- src/pages/CanvasSetupWAC.jsx
+++ src/pages/CanvasSetupWAC.jsx
@@ -1,17 +1,18 @@
 import { Canvas } from "@react-three/fiber";
-import React from "react";
+import DistortedFluidSphere from "../components/CombinedFluidSphere";
 import FluidBlob from "../components/FluidBlob";
-import { OrbitControls } from "@react-three/drei";
-import FluidSphere from "../components/FluidSphere";
 
 const CanvasSetupWAC = () => {
   return (
+    <div>
     <div style={{ width: "100vw", height: "100vh", background: "#050505" }}>
       <Canvas camera={{ position: [0, 0, 3] }}>
         {/* <FluidSphere /> */}
-        <FluidBlob/>
+        {/* <FluidBlob/> */}
         {/* <OrbitControls /> */}
+        <DistortedFluidSphere/>
       </Canvas>
+    </div>
     </div>
   );
 };
